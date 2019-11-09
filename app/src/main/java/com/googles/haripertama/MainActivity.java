e08c658094e82d26e9a0976cd44cd56da17818fe
teguhprasetyo <teguhprasetyo261211@gmail.com>
2019-11-09 20:29:06 +0700

    menambahkan view

---
+++ app/src/main/java/com/googles/haripertama/MainActivity.java
@@ -0,0 +1,14 @@
+package com.googles.haripertama;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.layout_login_ig );
+    }
+}
